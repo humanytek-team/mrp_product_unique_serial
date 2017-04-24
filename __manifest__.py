6ec591ff15d3c5c50da9f8758535ad9638ffbd9b
Manuel Marquez <manuel@hoatzin-mmarquez-laptop>
2017-04-23 22:54:05 -0400

    Initial commit: Template for addon.

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "mrp_product_unique_serial",
+    'summary': """
+        Extension of addon mrp for avoid asociate an product serial number to
+        more than one manufacturing order.""",
+    'description': """
+        Extension of addon mrp for avoid asociate an product serial number to
+        more than one manufacturing order.
+    """,
+    'author': "Humanytek",
+    'website': "http://www.humanytek.com",
+    'category': 'Manufacturing',
+    'version': '0.1.0',
+    'depends': ['mrp'],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
