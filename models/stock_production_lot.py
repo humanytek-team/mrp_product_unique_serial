cd67e70acd64026d7a1b2a19a25a0fac27021626
Manuel Márquez <manuel@humanytek.com>
2017-05-09 09:45:53 -0400

    [IMP] mrp_product_unique_serial: The search method of field available_for_product_in_workorder takes into account serials taken in another production order but not used in the workorder.

--- models/stock_production_lot.py
+++ models/stock_production_lot.py
@@ -34,25 +34,44 @@ class ProductionLot(models.Model):
         product_id = self.env.context['default_product_id']
         production_id = self.env.context['production_id']
         stock_production_lot_ids = self.search(
-            [('product_id', '=', product_id),
+            ['|',
+            '&',
+            ('product_id', '=', product_id),
             '|',
             ('stock_move_lots_ids', '=', False),
             '&',
             ('stock_move_lots_ids.production_id', '=', production_id),
-            ('stock_move_lots_ids.lot_produced_id', '=', False)])
+            ('stock_move_lots_ids.lot_produced_id', '=', False),
+            '&',
+            ('product_id', '=', product_id),
+            '&',
+            ('stock_move_lots_ids.production_id', '!=', production_id),
+            ('stock_move_lots_ids.lot_produced_id', '=', False),
+            ])
 
         lots_ids_available_for_product_in_workorder = []
         for stock_production_lot in stock_production_lot_ids:
             if stock_production_lot.stock_move_lots_ids:
-                done_wo = True
+                stock_move_lots_done_wo = True
+                stock_move_lots_quantity_done = 0.0
+                stock_move_lots_produced_id = False
                 for stock_move_lot in stock_production_lot.stock_move_lots_ids:
                     if not stock_move_lot.done_wo:
-                        done_wo = False
-                if done_wo:
+                        stock_move_lots_done_wo = False
+                    if stock_move_lot.quantity_done > 0:
+                        stock_move_lots_quantity_done = 1
+                    if stock_move_lot.lot_produced_id:
+                        stock_move_lots_produced_id = True
+                if not stock_move_lots_produced_id and \
+                    stock_move_lots_quantity_done == 0.0:
+                    lots_ids_available_for_product_in_workorder.append(
+                        stock_production_lot.id)
+                elif not stock_move_lots_produced_id and \
+                    stock_move_lots_done_wo:
                     lots_ids_available_for_product_in_workorder.append(
                         stock_production_lot.id)
             else:
                 lots_ids_available_for_product_in_workorder.append(
                     stock_production_lot.id)
 
-        return [('id', 'in', lots_ids_available_for_product_in_workorder)]
+        return [('id', 'in', sorted(lots_ids_available_for_product_in_workorder))]
