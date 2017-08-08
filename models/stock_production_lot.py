4abc614f8c38b19edcd10347e9a3f968ac6a74b5
Manuel Marquez <buzondemam@gmail.com>
2017-08-08 00:26:28 -0400

    [FIX] Fix error in form view of workorder on filter of series for the product finished that is manufactured.

--- models/stock_production_lot.py
+++ models/stock_production_lot.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-import logging
-_logger = logging.getLogger(__name__)
+
+
 class ProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
-    stock_move_lots_ids = fields.One2many(
-        'stock.move.lots', 'lot_id', 'Stock Moves', readonly=True)
     available_for_product_in_workorder = fields.Boolean(
         compute='_compute_available_for_product_in_workorder',
         search='_search_available_for_product_in_workorder'
     )
+    available_for_final_lot_in_workorder = fields.Boolean(
+        compute='_compute_available_for_product_in_workorder',
+        search='_search_available_for_final_lot_in_workorder'
+    )
 
-    @api.depends('stock_move_lots_ids')
     def _compute_available_for_product_in_workorder(self):
         for record in self:
             record.available_for_product_in_workorder = True
+            record.available_for_final_lot_in_workorder = True
 
     def _search_available_for_product_in_workorder(self, operator, value):
         """Function of searching for computed field
@@ -57,5 +59,27 @@ class ProductionLot(models.Model):
             ])
             if not lot_used:
                 product_lots_available_ids.append(lot.id)
+
+        return [('id', 'in', product_lots_available_ids)]
+
+    def _search_available_for_final_lot_in_workorder(self, operator, value):
+        """Function of searching for computed field
+        available_for_final_lot_in_workorder."""
+
+        product_id = self.env.context['default_product_id']
+        production_id = self.env.context['production_id']
+        StockMove = self.env['stock.move']
+        product_moves = StockMove \
+            .search([
+                ('product_id', '=', product_id),
+                ('production_id', '!=', False),
+                ('production_id', '!=', production_id),
+                ])
+        all_lots_reserved_ids = product_moves.mapped('lot_ids.id')
+
+        product_lots_available_ids = self.search([
+            ('product_id', '=', product_id),
+            ('id', 'not in', all_lots_reserved_ids)
+        ]).mapped('id')
 
         return [('id', 'in', product_lots_available_ids)]
